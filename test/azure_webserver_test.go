91a90509a25d0c84bb114592ab9868b16e7d6554
Ajaymorla1508 <your-email@example.com>
2025-06-12 23:49:36 -0400

    changes to the azure_webserver_test.go

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -2,35 +2,53 @@ package test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
-// You normally want to run this under a separate "Testing" subscription
-// For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "ec3ee3c5-b6d0-44db-98c1-49df6c18e116"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
 		TerraformDir: "../",
-		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "ajaymorla",
 		},
 	}
 
-	defer terraform.Destroy(t, terraformOptions)
+	defer func() {
+		t.Log(" Waiting before terraform destroy to avoid Azure race conditions...")
+		time.Sleep(30 * time.Second)
 
-	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+		t.Log("Attempting terraform destroy...")
+		_, err := terraform.DestroyE(t, terraformOptions)
+		if err != nil {
+			t.Logf("Terraform destroy failed: %v", err)
+			t.Log("Waiting 60s before retrying destroy...")
+			time.Sleep(60 * time.Second)
+			t.Log("Retrying terraform destroy...")
+			terraform.Destroy(t, terraformOptions)
+		} else {
+			t.Log("Terraform destroy succeeded.")
+		}
+
+		t.Log(" Final wait after destroy to ensure Azure releases all resources...")
+		time.Sleep(60 * time.Second)
+	}()
+
+	// Run `terraform init` and `terraform apply`
+	t.Log("Running terraform init and apply...")
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of output variable
+	// Get output variables from Terraform
+	t.Log("Fetching output variables...")
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 
-	// Confirm VM exists
-	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+	// Validate the VM exists
+	t.Logf("Verifying if VM %s exists in resource group %s...", vmName, resourceGroupName)
+	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID), "Expected VM to exist but it does not.")
 }
